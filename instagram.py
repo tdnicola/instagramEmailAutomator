d2b677dacaaef726010b036a95a8c99c07f26e42
tdnicola <tdnicola@gmail.com>
2020-03-31 15:59:40 -0400

    added search for my own email to be flagged for script. added writing of subject to a txt file. script also deletes email message after it has been downloaded.

--- instagram.py
+++ instagram.py
@@ -13,9 +13,9 @@ mail.login(emailAddress, password)
 
 mail.select("INBOX")
 
-
-result, data = mail.search(None, 'ALL')
-mailList = data[0].split()
+#finding emails sent from myself
+result, messageFromMe = mail.search(None, 'FROM', '"therealnicola@gmail.com"')
+mailList = messageFromMe[0].split()
 
 for item in mailList:
     result, data = mail.fetch(item, '(RFC822)')
@@ -26,6 +26,7 @@ for item in mailList:
     subject_ = emailMessage['Subject']
     date_ = emailMessage['Date']
     counter = 1
+    
     for part in emailMessage.walk():
         if part.get_content_maintype() == "multipart":
             continue
@@ -37,18 +38,16 @@ for item in mailList:
                 ext = '.bin'
             filename = 'msg-part-%08d%s' %(counter, ext)
         counter += 1
-    
     #saving the file
-    savePath = os.path.join(os.getcwd(),"emails", date_, subject_)
+    savePath = os.path.join(os.getcwd(),"emails", date_, )
     if not os.path.exists(savePath):
         os.makedirs(savePath)
     with open(os.path.join(savePath, filename), 'wb') as fp:
         fp.write(part.get_payload(decode=True))
-
-
-    print(subject_)
-    print(contentType)
-    if "plain" in contentType: 
-        print(part.get_payload())
-    else:
-        print(contentType)
+    #saving the subject as a text file
+    with open(os.path.join(savePath, 'text.txt'), 'w') as fp:
+        fp.write(subject_)
+        
+    # Change the flag to deleted and delete
+    typ, response = mail.store(item, '+FLAGS', r'(\Deleted)')
+    mail.expunge()
